54b43fed3d05eb1899a07b9966b24fa6eede3b5b
ShAndriy <andriyshchudlo@gmail.com>
2016-11-29 18:20:26 +0200

    abbreviation code

--- src/com/calendar/CalendarGeneration.java
+++ src/com/calendar/CalendarGeneration.java
@@ -2,46 +2,28 @@ package com.calendar;
 
 import java.time.*;
 
-import static java.time.temporal.TemporalAdjusters.*;
-
 public class CalendarGeneration {
 
-    public static int firstDayOfWeek(int year,int month, int day) {
-        LocalDate date = LocalDate.of(year, month, day);
-        int dayOfWeek = date.with(firstDayOfMonth()).getDayOfWeek().getValue();
-        return dayOfWeek;
-    }
-
-    public static boolean isLeapYear(int year) {
-        if ((year % 4 == 0) && (year % 100 != 0)) return true;
-        if (year % 400 == 0) return true;
-        return false;
-    }
-
-    public static int daysOfMonth(int month, int year) {
-        int february = 2;
-        int[] days = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        // check for leap year
-        if (month == february && isLeapYear(year)) days[month] = 29;
-        return days[month];
-    }
-
-    public static void Generation(int year, int month, int day) {
+    public void generate(int day, LocalDate date) {
         // String date = year + "-"+ month;
-        int first_day_of_week = CalendarGeneration.firstDayOfWeek(year, month,day);
-        int days_of_month = CalendarGeneration.daysOfMonth(month, year);
-        int days_week = 7;
+
+        int firstDayOfWeek = date.getDayOfWeek().getValue();
+        int daysInMonths = date.lengthOfMonth();
+
 
         Printer.titleCalendar();
 
-        for (int i = 0; i < first_day_of_week - 1; i++)
+        for (int i = 0; i < firstDayOfWeek - 1; i++)
             Printer.printSpace();
-        for (int i = 1; i <= days_of_month; i++) {
+
+        int days_week = 7;
+
+        for (int i = 1; i <= daysInMonths; i++) {
             if (i==day)
                 Colors.startBlueColor();
             Printer.printDayOfMonth(i);
             Colors.stopColor();
-            if (((i + first_day_of_week - 1) % days_week == 0) || (i == days_of_month)) Printer.printBlankOutput();
+            if (((i + firstDayOfWeek - 1) % days_week == 0) || (i == daysInMonths)) Printer.printBlankOutput();
         }
     }
 }
--- src/com/calendar/Main.java
+++ src/com/calendar/Main.java
@@ -1,16 +1,25 @@
 package com.calendar;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
 
         LocalDate date = LocalDate.now();
-        int month = date.getMonthValue();
-        int year = date.getYear();
+
         int day = date.getDayOfMonth();
 
-        CalendarGeneration.Generation(year, month, day);
+        CalendarGeneration calendar = new CalendarGeneration();
+        calendar.generate(day, date);
+
+
+        for (DayOfWeek d : DayOfWeek.values()) {
+            String name = d.getDisplayName(TextStyle.SHORT, Locale.getDefault());
+            System.out.printf(" %s ", name);
+        }
 
     }
 }
